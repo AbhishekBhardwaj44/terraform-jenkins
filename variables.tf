051147060387b9e04144abe1ad2c6b471c729a53
AbhishekBhardwaj44 <122599336+AbhishekBhardwaj44@users.noreply.github.com>
2024-03-07 19:20:52 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_secret_key" {
+  description="aws secret key"
+  type="string"
+  default=""
+}
+variable "aws_access_key_id" {
+  description="aws access key"
+  type="string"
+  default=""
+}
+variable "aws_region" {
+  description="aws region"
+  type="string"
+  default="ap-south-1"
+}
