201b6bbb6c2bb38d13923dbe70d0652d9c1c5bb0
AbhishekBhardwaj44 <122599336+AbhishekBhardwaj44@users.noreply.github.com>
2024-03-07 22:27:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
+
 provider "aws" {
-    region = "ap-south-1"
+    region = "ap-south-1"  
 }
-resource "aws_instance" "s3linux" {
+
+
+resource "aws_instance" "linux" {
   ami           = "ami-0ba259e664698cbfc"
   instance_type = "t2.micro"
   key_name      = "abhi-key"
 
   tags = {
-     Name = "hello-jenkins"
+    Name = "HelloWorld"
   }
+  
 }
