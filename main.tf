b8092c22d50a17880b419f4b2744a03851579dd0
AbhishekBhardwaj44 <122599336+AbhishekBhardwaj44@users.noreply.github.com>
2024-03-07 21:40:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 resource "aws_instance" "s3linux" {
-  ami           = ""
+  ami           = "ami-0ba259e664698cbfc"
   instance_type = "t2.micro"
   key_name      = "ec2-key"
 
